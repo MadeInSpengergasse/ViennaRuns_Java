1f673e9da6894f2c906a630d59bb26ad001051d3
Luca Weiss <WEI16416@spengergasse.at>
2016-10-03 17:02:17 +0200

    Enable java 1.8 & add Run+User models

--- src/main/java/domains/Run.java
+++ src/main/java/domains/Run.java
@@ -1,8 +1,76 @@
 package domains;
 
+import java.time.LocalDateTime;
+
 /**
  * Written by Luca Weiss (z3ntu)
  * https://github.com/z3ntu
  */
 public class Run {
+    private Integer id;
+    private User user;
+    private Float distance;
+    private Integer duration;
+    private LocalDateTime date;
+    private FeelingAfterRun feeling;
+
+    public Run() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Float distance) {
+        this.distance = distance;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public FeelingAfterRun getFeeling() {
+        return feeling;
+    }
+
+    public void setFeeling(FeelingAfterRun feeling) {
+        this.feeling = feeling;
+    }
+
+    public Run(Integer id, User user, Float distance, Integer duration, LocalDateTime date, FeelingAfterRun feeling) {
+        setId(id);
+        setUser(user);
+        setDistance(distance);
+        setDuration(duration);
+        setDate(date);
+        setFeeling(feeling);
+    }
 }
--- src/main/java/domains/User.java
+++ src/main/java/domains/User.java
@@ -5,4 +5,28 @@ package domains;
  * https://github.com/z3ntu
  */
 public class User {
+    private String name;
+    private String password;
+
+    public User(String name, String password) {
+        setName(name);
+        setPassword(password);
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
