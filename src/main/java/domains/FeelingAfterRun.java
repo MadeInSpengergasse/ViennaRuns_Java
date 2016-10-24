1bd58caff1a7eb5754057ade1b8d74bd18318838
Lukas Weber <lukas.weber@os-solutions.at>
2016-10-24 15:35:27 +0200

    pom.xml

--- src/main/java/domains/FeelingAfterRun.java
+++ src/main/java/domains/FeelingAfterRun.java
@@ -5,4 +5,27 @@ package domains;
  * https://github.com/z3ntu
  */
 public class FeelingAfterRun {
+    private Integer id;
+
+    private String feeling;
+
+    public FeelingAfterRun(Integer id, String feeling){
+
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id=id;
+    }
+
+    public String getFeeling(){
+        return feeling;
+    }
+
+    public void setFeeling(String feeling){
+        this.feeling=feeling;
+    }
+
 }
--- src/main/java/domains/User.java
+++ src/main/java/domains/User.java
@@ -4,5 +4,8 @@ package domains;
  * Written by Luca Weiss (z3ntu)
  * https://github.com/z3ntu
  */
-public class User {
+public abstract class User <DOMAIN_TYPE extends  User, PK_TYPE extends String> implements Comparable<DOMAIN_TYPE> {
+    private PK_TYPE id;
+
+    private Integer version;
 }
