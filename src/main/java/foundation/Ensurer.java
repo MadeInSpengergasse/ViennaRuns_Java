3d7ccac921179f148935fa4292f23fc23a5dfe54
Jan Wadsak <jan.wadsak@gmail.com>
2016-12-19 15:28:08 +0100

    code cleanup

--- src/main/java/foundation/Ensurer.java
+++ src/main/java/foundation/Ensurer.java
@@ -6,16 +6,12 @@ import org.apache.commons.lang3.StringUtils;
 public final class Ensurer {
 
     public static String IsNotBlank(String object, String name) {
-        if (StringUtils.isBlank(object)) {
-            throw new IllegalArgumentException(String.format("%s is blank", name));
-        }
+        if (StringUtils.isBlank(object)) throw new IllegalArgumentException(String.format("%s is blank", name));
         return object;
     }
 
     public static <T> T IsNotNull(T object, String name) {
-        if (object == null) {
-            throw new IllegalArgumentException(String.format("%s is null", name));
-        }
+        if (object == null) throw new IllegalArgumentException(String.format("%s is null", name));
         return object;
     }
 }
