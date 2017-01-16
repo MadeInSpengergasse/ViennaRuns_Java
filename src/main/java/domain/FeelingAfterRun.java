d1344bc7fbae504cf8df7ae58a2571c64ef9ff2a
Lukas Weber <lukas.weber@os-solutions.at>
2017-01-16 14:38:15 +0100

    Fixed compareTo of FeelingAfterRun

--- src/main/java/domain/FeelingAfterRun.java
+++ src/main/java/domain/FeelingAfterRun.java
@@ -30,7 +30,7 @@ public class FeelingAfterRun extends BaseModel<FeelingAfterRun, Long> {
     @Override
     public int compareTo(FeelingAfterRun o) {
         if (super._compareTo(o) == -1) return -1;
-        else if (o.getFeeling().equals(this.getFeeling()) && o.getId().equals(this.getId()) && o.getVersion().equals(this.getVersion()))
+        else if (o.getFeeling().equals(this.getFeeling()))
             return 0;
 
         else return -1;
