40a060d579eb944101f3af2e22b34ef1a4edfe4f
Lukas Weber <lukas.weber@os-solutions.at>
2017-02-20 14:49:05 +0100

    Initial commit

---
+++ src/main/java/io/github/spengergasse/ViennaRuns/ViennarunsApplication.java
@@ -0,0 +1,12 @@
+package io.github.spengergasse.ViennaRuns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViennarunsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViennarunsApplication.class, args);
+	}
+}
