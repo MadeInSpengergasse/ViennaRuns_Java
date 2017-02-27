0099c60025003607421668b5e394e6b73b5bfca6
Lukas Weber <lukas.weber@os-solutions.at>
2017-02-27 15:42:12 +0100

    Revert "basedomain.java"
    This reverts commit 64b946ec2d56e8aea7e19b1a74bfce2f36668414.

--- src/main/java/io/github/spengergasse/ViennaRuns/domain/BaseDomain.java
+++ src/main/java/io/github/spengergasse/ViennaRuns/domain/BaseDomain.java
@@ -7,56 +7,10 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Version;
 
-import io.github.spengergasse.ViennaRuns.foundation.ensurer;
-
-import java.time.LocalDateTime;
 @MappedSuperclass
 @Data
 @NoArgsConstructor
 public abstract class BaseDomain<DOMAIN_TYPE extends BaseDomain, PK_TYPE extends Number> extends AbstractPersistable<PK_TYPE> implements Comparable<DOMAIN_TYPE> {
-    private PK_TYPE id;
     @Version
     private Integer version;
-    private LocalDateTime createdAt;
-    private LocalDateTime lastModifiedAt;
-    private LocalDateTime deletedAt;
-
-    protected BaseModel() {
-        this.version = 0;
-    }
-
-    protected BaseModel(final PK_TYPE id, final Integer version) {
-        this.id = Ensurer.IsNotNull(id, "id");
-        this.version = Ensurer.IsNotNull(version, "version");
-    }
-
-    public final PK_TYPE getId() {
-        return id;
-    }
-
-    public final void setId(PK_TYPE id) {
-        this.id = Ensurer.IsNotNull(id, "id");
-    }
-
-    public final Integer getVersion() {
-        return version;
-    }
-
-    public final void setVersion(Integer version) {
-        this.version = Ensurer.IsNotNull(version, "version");
-    }
-
-    public final Boolean isNew() {
-        return id == null;
-    }
-
-    public final String toString() {
-        return String.format("%s{id:'%s', version:'%d'}", getClass().getSimpleName(),
-                id, version);
-    }
-
-    protected int _compareTo(BaseModel o) { //for intern usage
-        if (o.getId().equals(this.getId()) && o.getVersion().equals(this.getVersion())) return 0;
-        else return -1;
-    }
 }
